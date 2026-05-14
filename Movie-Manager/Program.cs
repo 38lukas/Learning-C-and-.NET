29c46625f274536056f30e8509e8634c4584e92c
Lukas Stieneke <lukas.stieneke@gmail.com>
2026-05-14 07:32:02 +0200

    Added feature for getting and printing movies after 2005

--- Movie-Manager/Program.cs
+++ Movie-Manager/Program.cs
@@ -2,6 +2,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        // Create a list of movies
         var movies = new List<Movie>
         {
             new Movie { Title = "The Shawshank Redemption", Year = 1994, Rating = 9.3 },
@@ -10,5 +11,18 @@ public class Program
             new Movie { Title = "Pulp Fiction", Year = 1994, Rating = 8.9 },
             new Movie { Title = "Inception", Year = 2010, Rating = 8.8 },
         };
+
+        // Get the movies after 2005 by rating
+        var moviesAfter2010ByRating = movies
+            .Where(m => m.Year > 2005)
+            .OrderByDescending(m => m.Rating)
+            .ToList();
+
+        // Print the movies after 2005 by rating
+        Console.WriteLine("Movies after 2005 by rating:");
+        foreach (var movie in moviesAfter2010ByRating)
+        {
+            Console.WriteLine($"{movie.Title}: {movie.Rating}");
+        }
     }
 }
